96b49b4c38d219ffe80216dc778e8113f339c387
oceaann <47253870+oceaann@users.noreply.github.com>
2021-08-27 00:32:40 +0200

    deno formater

--- src/constants.js
+++ src/constants.js
@@ -1,8 +1,8 @@
 export const OP_CODES = {
-    EVENTS: 0,
-    HELLO: 10,
-    HEARTBEAT: 1,
-    IDENTIFY: 2,
-    HEARTBEAT_ACK: 11,
-    RESUME: 6
-}
+  EVENTS: 0,
+  HELLO: 10,
+  HEARTBEAT: 1,
+  IDENTIFY: 2,
+  HEARTBEAT_ACK: 11,
+  RESUME: 6,
+};
--- tests/debug.js
+++ tests/debug.js
@@ -1,18 +1,21 @@
-import { Gateway } from "../src/gateway/gateway.js"
-import "https://deno.land/x/dotenv/load.ts"
+import { Gateway } from "../src/gateway/gateway.js";
+import "https://deno.land/x/dotenv/load.ts";
 
-const connection = new Gateway(Deno.env.get("TOKEN"))
+const connection = new Gateway(Deno.env.get("TOKEN"));
 connection.connect({
-    intents: 32509,
-    properties: {
-        $os: Deno.build.$os,
-        $browser: "Deno",
-        $device: "Deno",
-    },
-    compress: false
-})
+  intents: 32509,
+  properties: {
+    $os: Deno.build.$os,
+    $browser: "Deno",
+    $device: "Deno",
+  },
+  compress: false,
+});
 
-connection.ws.addEventListener("message", (d) => console.log(JSON.parse(d.data)))
+connection.ws.addEventListener(
+  "message",
+  (d) => console.log(JSON.parse(d.data)),
+);
 
 /*
 setTimeout(() => {
