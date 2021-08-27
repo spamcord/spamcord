02b8b460307f3b7f7d9d02b36e62c130da98c042
oceaann <47253870+oceaann@users.noreply.github.com>
2021-08-27 12:51:47 +0200

    Ratelimit model

--- src/rest/retelimit.js
+++ src/rest/retelimit.js
@@ -1,3 +1,16 @@
 export class RateLimit {
+  constructor(data) {
+    this.global = data.global;
+    this.limit = data.limit;
+    this.bucket = data.bucket;
+    this.reset = data.rest;
 
+    this.request = {
+      url: data.request.url,
+      body: data.request.body,
+      headers: data.request.headers,
+    };
+
+    this.key = data.key;
+  }
 }
