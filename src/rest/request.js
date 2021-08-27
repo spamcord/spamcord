fbb23f8fda4e08dcd4a75a3da9bbd37372cb3a9c
oceaann <47253870+oceaann@users.noreply.github.com>
2021-08-27 14:43:20 +0200

    (fix): add missing return
    https://github.com/spamcord/spamcord/runs/3443181661

--- src/rest/request.js
+++ src/rest/request.js
@@ -8,9 +8,10 @@ export class Request {
     let results;
     // deno-fmt-ignore
     results = url.replace(/\/([a-z-]+)\/(?:[0-9]{17,19})/g, (match, p) => (p === "channels" || p === "guilds" || p === "webhooks" ? match : `/${p}/:id`)) // deno-lint-ignore
-            .replace(/\/reactions\/[^/]+/g, "/reactions/:id")
-            .replace(/\/reactions\/:id\/[^/]+/g,"/reactions/:id/:userID")
-            .replace(/^\/webhooks\/(\d+)\/[A-Za-z0-9-_]{64,}/, "/webhooks/$1/:token")
+        .replace(/\/reactions\/[^/]+/g, "/reactions/:id")
+        .replace(/\/reactions\/:id\/[^/]+/g,"/reactions/:id/:userID")
+        .replace(/^\/webhooks\/(\d+)\/[A-Za-z0-9-_]{64,}/, "/webhooks/$1/:token")
     if (method === "DELETE") results = `${method} DELETE`;
+    return results;
   }
 }
