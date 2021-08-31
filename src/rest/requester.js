b201d83ee514cf21a47a678f70a3508c47fe64a8
oceaann <47253870+oceaann@users.noreply.github.com>
2021-08-31 14:59:58 +0200

    request handler init

---
+++ src/rest/requester.js
@@ -0,0 +1,31 @@
+import {Queue} from "./queue.js";
+
+class RequestHandler {
+
+    #token
+
+    constructor() {
+        this.queues = {}
+    }
+
+    async request(url, method, body) {
+
+        const key = this.simplify(url, method)
+
+        if(!this.queues[key]) {
+            this.queues[key] = new Queue()
+        }
+
+        return await this.queues[key].push(url, method, body)
+
+    }
+
+    simplify(url, method) {
+        let res = url.replace(/\/([a-z-]+)\/(?:[0-9]{17,19})/g, (match, p) => p === "channels" || p === "guilds" || p === "webhooks" ? match : `/${p}/:id`).replace(/\/reactions\/[^/]+/g, "/reactions/:id").replace(/\/reactions\/:id\/[^/]+/g,"/reactions/:id/:userID").replace(/^\/webhooks\/(\d+)\/[A-Za-z0-9-_]{64,}/, "/webhooks/$1/:token")
+        if(method === "DELETE") {
+            res = `${res} D`
+        }
+        return res
+    }
+
+}
