f824d92da3af3af955345fab7f363bdc7f7eed0e
oceaann <47253870+oceaann@users.noreply.github.com>
2021-08-31 15:30:21 +0200

    (fix): remove await from promise callback
    + remove await from promise callback
    + remove useless console.log

--- src/rest/queue.js
+++ src/rest/queue.js
@@ -1,62 +1,61 @@
-import {wait} from "../utlis.js";
+import { wait } from "../utlis.js";
 
 export class Queue {
-    #requests;
+  #requests;
 
-    constructor() {
-        this.#requests = [];
-        this.isRunning = false;
-    }
+  constructor() {
+    this.#requests = [];
+    this.isRunning = false;
+  }
 
-    push(url, method, body) {
-        console.log(url);
+  push(url, method, body) {
 
-        return new Promise(async (resolve, reject) => {
-            this.#requests.push({url, method, body, resolve, reject});
+    return new Promise((resolve, reject) => {
+      this.#requests.push({ url, method, body, resolve, reject });
 
-            if (!this.isRunning) {
-                this.isRunning = true;
-                await this.execute();
-            }
-        });
-    }
+      if (!this.isRunning) {
+        this.isRunning = true;
+        this.execute();
+      }
+    });
+  }
 
-    async execute() {
-        while (this.#requests.length !== 0) {
-            const r = this.#requests[0];
-            let res;
+  async execute() {
+    while (this.#requests.length !== 0) {
+      const r = this.#requests[0];
+      let res;
 
-            try {
-                res = await this.fetch(r.url, r.method, r.body);
-            } catch (e) {
-                await r.reject(e);
-            }
+      try {
+        res = await this.fetch(r.url, r.method, r.body);
+      } catch (e) {
+        await r.reject(e);
+      }
 
-            if ([429].includes(res.status)) {
-                const response = await res.json();
+      if ([429].includes(res.status)) {
+        const response = await res.json();
 
-                if (!response.retry_after) {
-                    await r.reject("Unknown rate limit time");
-                }
-
-                await wait(response.retry_after || 0);
-            } else {
-                this.#requests.shift();
-                await r.resolve(res);
-            }
-
-            await wait(50);
+        if (!response.retry_after) {
+          await r.reject("Unknown rate limit time");
         }
-    }
 
-    fetch(url, method, body, headers = {}) {
-        return fetch(url, {
-            method,
-            headers: {
-                "content-type": "application/json",
-                ...headers,
-            },
-            body,
-        });
+        await wait(response.retry_after || 0);
+      } else {
+        this.#requests.shift();
+        await r.resolve(res);
+      }
+
+      await wait(50);
     }
+  }
+
+  fetch(url, method, body, headers = {}) {
+    return fetch(url, {
+      method,
+      headers: {
+        "content-type": "application/json",
+        ...headers,
+      },
+      body,
+    });
+  }
 }
