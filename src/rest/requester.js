f66627de706b3eb5f34c9cf1413ca0694a06ae8c
oceaann <47253870+oceaann@users.noreply.github.com>
2021-08-31 15:28:47 +0200

    (temp): remove not used declaration

--- src/rest/requester.js
+++ src/rest/requester.js
@@ -1,28 +1,26 @@
-import {Queue} from "./queue.js";
+import { Queue } from "./queue.js";
 
-class RequestHandler {
-    #token;
+export class RequestHandler {
+  constructor() {
+    this.queues = {};
+  }
 
-    constructor() {
-        this.queues = {};
+  async request(url, method, body) {
+    const key = this.simplify(url, method);
+
+    if (!this.queues[key]) {
+      this.queues[key] = new Queue();
     }
 
-    async request(url, method, body) {
-        const key = this.simplify(url, method);
+    return await this.queues[key].push(url, method, body);
+  }
 
-        if (!this.queues[key]) {
-            this.queues[key] = new Queue();
-        }
-
-        return await this.queues[key].push(url, method, body);
+  simplify(url, method) {
+    // deno-fmt-ignore
+    let res = url.replace(/\/([a-z-]+)\/(?:[0-9]{17,19})/g, (match, p) => p === "channels" || p === "guilds" || p === "webhooks" ? match : `/${p}/:id`).replace(/\/reactions\/[^/]+/g, "/reactions/:id").replace(/\/reactions\/:id\/[^/]+/g, "/reactions/:id/:userID").replace(/^\/webhooks\/(\d+)\/[A-Za-z0-9-_]{64,}/, "/webhooks/$1/:token").replace(/^\/webhooks\/(\d+)\/[A-Za-z0-9-_]{64,}/, "/webhooks/$1/:token");
+    if (method === "DELETE") {
+      res = `${res} D`;
     }
-
-    simplify(url, method) {
-        // deno-fmt-ignore
-        let res = url.replace(/\/([a-z-]+)\/(?:[0-9]{17,19})/g, (match, p) => p === "channels" || p === "guilds" || p === "webhooks" ? match : `/${p}/:id`).replace(/\/reactions\/[^/]+/g, "/reactions/:id").replace(/\/reactions\/:id\/[^/]+/g, "/reactions/:id/:userID").replace(/^\/webhooks\/(\d+)\/[A-Za-z0-9-_]{64,}/, "/webhooks/$1/:token").replace(/^\/webhooks\/(\d+)\/[A-Za-z0-9-_]{64,}/, "/webhooks/$1/:token");
-        if (method === "DELETE") {
-            res = `${res} D`;
-        }
-        return res;
-    }
+    return res;
+  }
 }
